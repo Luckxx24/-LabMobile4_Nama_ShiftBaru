50b8e323302917f1fc52888bb59b2adad22023e6
Luckxx24 <127165929+Luckxx24@users.noreply.github.com>
2024-09-30 20:31:18 +0700

    conductor
    conductor conductor we have problem

---
+++ lib/ui/produk_detail.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import '/model/produk.dart';
+import '/ui/produk_form.dart';
+
+class ProdukDetail extends StatefulWidget {
+  Produk? produk;
+
+  ProdukDetail({Key? key, this.produk}) : super(key: key);
+
+  @override
+  _ProdukDetailState createState() => _ProdukDetailState();
+}
+
+class _ProdukDetailState extends State<ProdukDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail Produk Lucky'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Kode : ${widget.produk!.kodeProduk}",
+              style: const TextStyle(fontSize: 20.0),
+            ),
+            Text(
+              "Nama : ${widget.produk!.namaProduk}",
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            Text(
+              "Harga : Rp. ${widget.produk!.hargaProduk.toString()}",
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(height: 20),
+            _tombolHapusEdit(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _tombolHapusEdit() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        // Tombol Edit
+        OutlinedButton(
+          child: const Text("EDIT"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ProdukForm(
+                  produk: widget.produk!,
+                ),
+              ),
+            );
+          },
+        ),
+        const SizedBox(width: 10), // Jarak antara tombol
+        // Tombol Hapus
+        OutlinedButton(
+          child: const Text("DELETE"),
+          onPressed: () => confirmHapus(),
+        ),
+      ],
+    );
+  }
+
+  void confirmHapus() {
+    AlertDialog alertDialog = AlertDialog(
+      content: const Text("Yakin ingin menghapus data ini?"),
+      actions: [
+        // Tombol hapus
+        OutlinedButton(
+          child: const Text("Ya"),
+          onPressed: () {
+            // Implementasi logika hapus data
+            Navigator.pop(context); // Kembali setelah konfirmasi
+          },
+        ),
+        // Tombol batal
+        OutlinedButton(
+          child: const Text("Batal"),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (context) => alertDialog,
+    );
+  }
+}
